b1c7d22390cd5fa90acaa09c74742d46c9e760e8
gtxaspec <gtxent@gmail.com>
2026-07-18 15:02:37 -0700

    tests: clang-format main.c

--- tests/main.c
+++ tests/main.c
@@ -13,14 +13,14 @@ GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv)
 {
-	GREATEST_MAIN_BEGIN();
-	RUN_SUITE(config_suite);
-	RUN_SUITE(util_suite);
-	RUN_SUITE(json_suite);
-	RUN_SUITE(http_suite);
-	RUN_SUITE(file_suite);
-	RUN_SUITE(sei_suite);
-	RUN_SUITE(jpeg_suite);
-	RUN_SUITE(aac_suite);
-	GREATEST_MAIN_END();
+    GREATEST_MAIN_BEGIN();
+    RUN_SUITE(config_suite);
+    RUN_SUITE(util_suite);
+    RUN_SUITE(json_suite);
+    RUN_SUITE(http_suite);
+    RUN_SUITE(file_suite);
+    RUN_SUITE(sei_suite);
+    RUN_SUITE(jpeg_suite);
+    RUN_SUITE(aac_suite);
+    GREATEST_MAIN_END();
 }
